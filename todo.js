3db888f7aab40509e92487c0e2361b7ff366e8dd
Mrunal <44579222+mruna7@users.noreply.github.com>
2020-11-03 11:38:46 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -28,7 +28,16 @@ function Form_Processes() {
 		} else {
 			console.log('ERROR');
 		}
-	
+	}
+
+	function generate_id() {
+  		var text = "";
+  		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+
+  		for (var i = 0; i < 8; i++){
+    		text += possible.charAt(Math.floor(Math.random() * possible.length));
+  		}
+  		return text;
 	}
 
 }
