14989217e714bd348130ff0ae819581276f81769
Mrunal <44579222+mruna7@users.noreply.github.com>
2020-11-03 10:56:45 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -27,18 +27,9 @@ function Form_Processes() {
 			itemToRemove.outerHTML = "";
 			this.count -= 1;
 		} else {
-			console.log('ERROR: Not Enough Child Elements to remove (script.js:25)');
+			console.log('ERROR');
 		}
-	}
-
-	function generate_id() {
-  		var text = "";
-  		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
-
-  		for (var i = 0; i < 8; i++){
-    		text += possible.charAt(Math.floor(Math.random() * possible.length));
-  		}
-  		return text;
+	
 	}
 
 }
